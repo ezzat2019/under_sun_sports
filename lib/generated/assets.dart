90430a9e79c84fd0dc014fa05a0b4dbe9ef78e12
programmer_ezzat <coezz@yahoo.com>
2021-03-23 01:58:38 -0700

    final v5

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -2,6 +2,7 @@
 class Assets {
   Assets._();
 
-  static const String iconUnderSun = 'assets/icon/under_sun.png';
+  static const String iconUnderSun2 = 'assets/icon/under_sun2.png';
+  static const String iconUnderSun30 = 'assets/icon/under_sun30.png';
 
 }
